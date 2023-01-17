3cf924d7f9482db4c4847962ad825cf2cf3fc19b
muncaem <mooncm02@gmail.com>
2023-01-17 16:25:17 +0900

    feed ui kotlin val connect

--- app/src/main/java/com/example/once/Feed.kt
+++ app/src/main/java/com/example/once/Feed.kt
@@ -5,15 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ScrollView
+import android.widget.TextView
 
 class Feed : Fragment() {
+
+    lateinit var feedScrollView: ScrollView //피드 페이지의 스크롤뷰
+    lateinit var feedNameText: TextView //피드 페이지의 닉네임 텍스트뷰
+    lateinit var feedImageButton: ImageButton //피드 페이지의 이미지버튼
+    lateinit var feedLikeButton: ImageButton //피드 페이지의 좋아요 이미지버튼
+    lateinit var feedTotalLikeText: TextView //피드 페이지의 좋아요 합계 텍스트뷰
 
     @Override
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.feed, container, false)
+        val view = inflater.inflate(R.layout.feed, container, false)
+
+        feedScrollView = view.findViewById(R.id.feed_scrollView)
+        feedNameText = view.findViewById(R.id.feed_nickTxt)
+        feedImageButton = view.findViewById(R.id.feed_imageBtn)
+        feedLikeButton = view.findViewById(R.id.feed_likeBtn)
+        feedTotalLikeText = view.findViewById(R.id.feed_totalLikeTxt)
+
+        //return inflater.inflate(R.layout.feed, container, false)
+
+        return view
     }
 }
