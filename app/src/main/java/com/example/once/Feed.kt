e5ea4ed128fb54eb179e6c415732e8bf6448daaf
muncaem <mooncm02@gmail.com>
2023-01-17 18:12:54 +0900

    feed1

--- app/src/main/java/com/example/once/Feed.kt
+++ app/src/main/java/com/example/once/Feed.kt
@@ -1,5 +1,9 @@
 package com.example.once
 
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,6 +15,8 @@ import android.widget.ScrollView
 import android.widget.TextView
 
 class Feed : Fragment() {
+
+    //lateinit var myHelper: myDBHelper //mainactivity에서 생성한 db이름
 
     lateinit var feedScrollView: ScrollView //피드 페이지의 스크롤뷰
     lateinit var feedNameText: TextView //피드 페이지의 닉네임 텍스트뷰
@@ -30,9 +36,32 @@ class Feed : Fragment() {
         feedImageButton = view.findViewById(R.id.feed_imageBtn)
         feedLikeButton = view.findViewById(R.id.feed_likeBtn)
         feedTotalLikeText = view.findViewById(R.id.feed_totalLikeTxt)
-
         //return inflater.inflate(R.layout.feed, container, false)
+
+        //db연결
+        //sqlDB = myHelper.readableDatabase
+        //var cursor: Cursor
+        //cursor = sqlDB.rawQuery("SELECT * FROM diaryTBL;", null)
+        //var strNames = "닉네임" + "\r\n" + "--------" + "\r\n"
+        //var strLikes = "좋아요" + "\r\n" + "--------" + "\r\n"
+        //var strImage = "이미지" + "\r\n" + "--------" + "\r\n"
+        //var strContents = "내용" + "\r\n" + "--------" + "\r\n"
+        //while (cursor.moveToNext()) {
+            //strNames += cursor.getString(0) + "\r\n"
+            //strNumbers += cursor.getString(1) + "\r\n"
+        //}
+        //feedNameText.setText(strNames)
+        //feedTotalLikeText.setText(strLikes)
+        //feedImageButton.setText(strImage)
+        //cursor.close()
+        //sqlDB.close()
+
+        //피드에서 일기의 이미지를 클릭하면
+        feedImageButton.setOnClickListener{
+            //특정 일기 fragment로 이동
+        }
 
         return view
     }
+
 }
