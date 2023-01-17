3dee95e3e1a17d256d197faf8e6fd168889a5431
Dodogari <87838377+Dodogari@users.noreply.github.com>
2023-01-17 13:45:03 +0900

    bottomNavBar
    fragment change success

--- app/src/main/java/com/example/once/MainActivity.kt
+++ app/src/main/java/com/example/once/MainActivity.kt
@@ -1,19 +1,50 @@
 package com.example.once
 
 import android.os.Bundle
+import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
-    var bottomNavigationView: BottomNavigationView? = null
+
+    lateinit var bottomNav : BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadFragment(Fragment1())
+        bottomNav = findViewById(R.id.bottomNavi) as BottomNavigationView
 
-        bottomNavigationView = findViewById(R.id.bottomNavi)
+        bottomNav.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.item_feed -> {
+                    loadFragment(Fragment1())
+                    true
+                }
+                R.id.item_record -> {
+                    loadFragment(Fragment2())
+                    true
+                }
+                R.id.item_mypage -> {
+                    loadFragment(Fragment3())
+                    true
+                }
+                R.id.item_search -> {
+                    loadFragment(Fragment4())
+                    true
+                }
+                else -> {
+                    true
+                }
+            }
+        }
 
-
+    }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.main_frame, fragment)
+        transaction.commit()
     }
 }
