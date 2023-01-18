c54edf3d3c0e4f91429e91a4e6e15626461e879a
Dodogari <87838377+Dodogari@users.noreply.github.com>
2023-01-18 11:12:40 +0900

    record UI
    menu button, diary ui

---
+++ app/src/main/java/com/example/once/Diary.kt
@@ -0,0 +1,24 @@
+package com.example.once
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import java.io.File
+
+class Diary : Fragment() {
+    private val OPEN_GALLERY = 1
+    @Override
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.diary, container, false)
+    }
+}
