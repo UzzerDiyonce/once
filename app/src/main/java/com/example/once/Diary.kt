96f96f0827abc568b205017482e8d4a1266e53a0
Dodogari <87838377+Dodogari@users.noreply.github.com>
2023-01-18 15:33:17 +0900

    sample
    sss

--- app/src/main/java/com/example/once/Diary.kt
+++ app/src/main/java/com/example/once/Diary.kt
@@ -4,21 +4,35 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
+import android.os.PersistableBundle
 import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import java.io.File
 
-class Diary : Fragment() {
-    private val OPEN_GALLERY = 1
+class Diary : AppCompatActivity() {
+
+    lateinit var uploadBtn: Button
+    lateinit var imgView: ImageView
+
     @Override
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.diary, container, false)
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.diary)
+
+        uploadBtn = findViewById(R.id.drawing)
+        imgView = findViewById(R.id.pic)
+
+        uploadBtn.setOnClickListener(View.OnClickListener {
+            fun onClick(){
+                
+            }
+        });
     }
 }
