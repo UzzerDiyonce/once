e4247fece36395c90f9fd4f51ef8f8b1add04297
최하연 <whitelotus21@naver.com>
2023-01-19 09:23:58 +0900

    로그아웃 버튼

--- app/src/main/java/com/example/once/MyPage.kt
+++ app/src/main/java/com/example/once/MyPage.kt
@@ -5,8 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
 
 class MyPage : Fragment() {
+
+    lateinit var mypageDibtn : Button
+    lateinit var mypageCapbtn : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //하단 네비게이션 바 구현
+        setMypageFragment(MyPage_Diary())
+
+        mypageDibtn.setOnClickListener() {
+            setMypageFragment(MyPage_Diary())
+        }
+        mypageCapbtn.setOnClickListener(){
+            setMypageFragment(MyPage_Capsule())
+        }
+    }
 
     @Override
     override fun onCreateView(
@@ -16,5 +34,11 @@ class MyPage : Fragment() {
         val mypage_view = inflater.inflate(R.layout.my_page, container, false)
 
         return mypage_view
+    }
+
+    public fun setMypageFragment(fragment: Fragment){
+        val transaction = childFragmentManager.beginTransaction()
+        transaction.replace(R.id.mypage_container, fragment)
+        transaction.commit()
     }
 }
