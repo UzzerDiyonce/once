3e0080609ed0c9a95a2588075ef7b7649c7ab60c
muncaem <mooncm02@gmail.com>
2023-01-20 00:07:44 +0900

    modifying

--- app/src/main/java/com/example/once/AlaramAcitivity.kt
+++ app/src/main/java/com/example/once/AlaramAcitivity.kt
@@ -2,10 +2,13 @@ package com.example.once
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class AlaramAcitivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alaram_acitivity)
+
     }
 }
