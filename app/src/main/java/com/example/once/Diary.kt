34d88e7fac3d1399e9c28f29c30a6383e3633164
Dodogari <87838377+Dodogari@users.noreply.github.com>
2023-01-19 21:46:20 +0900

    sibal ue andoe
    ssakki

--- app/src/main/java/com/example/once/Diary.kt
+++ app/src/main/java/com/example/once/Diary.kt
@@ -1,17 +1,27 @@
 package com.example.once
 
+<<<<<<< Updated upstream
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+=======
+import android.os.Bundle
+import android.os.PersistableBundle
+>>>>>>> Stashed changes
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+<<<<<<< Updated upstream
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+=======
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+>>>>>>> Stashed changes
 
 class Diary : Fragment() {
 
@@ -22,6 +32,7 @@ class Diary : Fragment() {
     @Override
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+<<<<<<< Updated upstream
 
 
     }
@@ -32,5 +43,17 @@ class Diary : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.diary, container, false)
+=======
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+
+>>>>>>> Stashed changes
     }
 }
---
+++ app/src/main/java/com/example/once/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.example.once
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val handler = android.os.Handler()
+        handler.postDelayed(Runnable {
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent) //Loagin화면을 띄운다.
+            finish() //현재 액티비티 종료
+        }, 2000)
+    }
+}
