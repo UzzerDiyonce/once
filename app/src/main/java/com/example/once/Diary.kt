eb62170f4359d67b27b239bbe9360f2385035e90
Dodogari <87838377+Dodogari@users.noreply.github.com>
2023-01-19 21:22:15 +0900

    temporary push
    temporary

--- app/src/main/java/com/example/once/Diary.kt
+++ app/src/main/java/com/example/once/Diary.kt
@@ -1,27 +1,31 @@
 package com.example.once
 
-import android.content.Intent
-import android.net.Uri
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.os.Environment
-import android.os.PersistableBundle
-import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
-import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
-import java.io.File
+import androidx.core.content.ContextCompat
 
 class Diary : Fragment() {
 
-    lateinit var uploadBtn: Button
-    lateinit var imgView: ImageView
+    lateinit var uploadBtn: Button                      //이미지 업로드 버튼
+    lateinit var imgView: ImageView                     //이미지 미리보기 뷰
+    private var uri: String? = null                     //이미지 경로
 
     @Override
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
--- app/src/main/java/com/example/once/MainActivity.kt
+++ app/src/main/java/com/example/once/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.once
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -12,6 +17,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var bottomNav : BottomNavigationView           //하단 네비게이션 바 변수
     lateinit var diary: FloatingActionButton
     lateinit var timecapsule: FloatingActionButton
+
+    private var REQUEST_READ_EXTERNAL_STORAGE = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +32,27 @@ class MainActivity : AppCompatActivity() {
 
         diary.setVisibility(View.INVISIBLE)
         timecapsule.setVisibility(View.INVISIBLE)
+
+        //앱에 이미지 접근 권한을 부여하였는지 확인
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+
+            //권한이 허용되지 않았다면
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                //이전에 거부한 적이 있다면 권한이 필요한 이유를 설명한다.
+                var dlg = AlertDialog.Builder(this)
+                dlg.setTitle("갤러리 접근 권한 필요")
+                dlg.setMessage("갤러리에서 사진을 첨부하고자 할 때 외부 저장소 권한이 필수로 필요합니다.")
+                dlg.setPositiveButton("확인"){ dialog, which -> ActivityCompat.requestPermissions(this@MainActivity,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_READ_EXTERNAL_STORAGE) }
+                dlg.setNegativeButton("취소", null)
+                dlg.show()
+            } else {
+                // 처음 권한 요청
+                ActivityCompat.requestPermissions(this@MainActivity,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_READ_EXTERNAL_STORAGE)
+            }
 
         //네비게이션 바 메뉴 누를 시 액션
         bottomNav.setOnItemSelectedListener {
