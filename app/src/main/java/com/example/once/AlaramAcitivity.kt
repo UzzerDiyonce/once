29a80362a087488b4b01fefdf53dbfa526dc416d
muncaem <mooncm02@gmail.com>
2023-01-21 01:38:47 +0900

    code connect alaram func1

--- app/src/main/java/com/example/once/AlaramAcitivity.kt
+++ app/src/main/java/com/example/once/AlaramAcitivity.kt
@@ -1,14 +1,22 @@
 package com.example.once
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 
 class AlaramAcitivity : AppCompatActivity() {
+
+    lateinit var backBtn: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alaram_acitivity)
 
+        backBtn = findViewById(R.id.leftArrowBtn)
+        backBtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
