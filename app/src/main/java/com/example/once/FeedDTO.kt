6bad8935b1b29296c5f1bf7778cf0f6fa57b9d3f
muncaem <mooncm02@gmail.com>
2023-01-22 22:57:11 +0900

    add data var for database connection

--- app/src/main/java/com/example/once/FeedDTO.kt
+++ app/src/main/java/com/example/once/FeedDTO.kt
@@ -1,8 +1,19 @@
 package com.example.once
 
 data class FeedDTO (
-    var destinationUid: String? = null,
-    var userId: String? = null,
-    var uid: String? = null,
-    var kind: Int? = null,
-)
+    var userId: String? = null, //이메일
+    var uid: String? = null, //uid
+    var timestamp: Long? = null, //timestamp
+    var title: String? = null, //작성제목
+    var contents: String? = null, //작성내용
+    var imageUrl: String? = null, //업로드이미지
+    //세부내용 피드
+    var weather_kind: Int? = null, //날씨종류 0: 맑음 | 1: 흐림 | 2: 비 | 3: 눈
+) {
+    data class Comment(
+        var userId: String? = null, //이메일
+        var uid: String? = null, //uid
+        var comment: String? = null, //댓글
+        var timestamp: Long? = null //timestamp
+    )
+}
