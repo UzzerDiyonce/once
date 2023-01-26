f5d98ab08b5b5a8b36ecb4bb5b662b22a0930b96
muncaem <mooncm02@gmail.com>
2023-01-26 17:28:36 +0900

    build comment recyclerview

--- app/src/main/java/com/example/once/CommentActivity.kt
+++ app/src/main/java/com/example/once/CommentActivity.kt
@@ -2,10 +2,24 @@ package com.example.once
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class CommentActivity : AppCompatActivity() {
+    lateinit var commentBackBtn: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_comment)
+
+        //메인페이지로 돌아가기 버튼
+        commentBackBtn = findViewById(R.id.commentBack)
+        commentBackBtn.setOnClickListener {
+            onBackPressed()
+        }
+
+    }
+    //메인 돌아가기 함수
+    override fun onBackPressed() {
+        super.onBackPressed()
     }
 }
