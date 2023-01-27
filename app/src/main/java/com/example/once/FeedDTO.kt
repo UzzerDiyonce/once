824a6f6923df0dae19bbb730077b79cb89be0c56
muncaem <mooncm02@gmail.com>
2023-01-28 07:01:10 +0900

    fail setting nick func

--- app/src/main/java/com/example/once/FeedDTO.kt
+++ app/src/main/java/com/example/once/FeedDTO.kt
@@ -3,6 +3,7 @@ package com.example.once
 data class FeedDTO (
     var userId: String? = null, //이메일
     var uid: String? = null, //uid
+    var nickname: String? = null, //닉네임
     var profileImageUrl: String? = null, //프로필이미지
     var timestamp: Long? = null, //timestamp
     var title: String? = null, //작성제목
